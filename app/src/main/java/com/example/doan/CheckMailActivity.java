1574106a3684937e4f3757e23fcdc547aab474e7
Khang Vũ <128994152+SleepySouls@users.noreply.github.com>
2024-11-19 15:32:41 +0700

    another backup

---
+++ app/src/main/java/com/example/doan/CheckMailActivity.java
@@ -0,0 +1,23 @@
+package com.example.doan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CheckMailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_mail);
+
+
+    }
+}
--- app/src/main/java/com/example/doan/ForgotPwdActivity.java
+++ app/src/main/java/com/example/doan/ForgotPwdActivity.java
@@ -1,25 +1,39 @@
 package com.example.doan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.common.api.Api;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
 
 public class ForgotPwdActivity extends AppCompatActivity {
     private EditText etRecoveryEmail;
     private Button btnConfirmEmail;
 
+    class ApiResponse{
+        boolean success;
+        String message;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.forgot_pwd);
+        setContentView(R.layout.forgot_passwd);
         etRecoveryEmail = findViewById(R.id.recovery_email);
         btnConfirmEmail = findViewById(R.id.btn_for_confirm_email);
         btnConfirmEmail.setOnClickListener(v -> {
@@ -27,6 +41,55 @@ public class ForgotPwdActivity extends AppCompatActivity {
             if (recoveryEmail.isEmpty()) {
                 Toast.makeText(ForgotPwdActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
             }
+            else{
+                sendRecoveryEmail(recoveryEmail);
+            }
         });
+    }
+
+    private void sendRecoveryEmail(String recoveryEmail) {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://potholescannerapi.onrender.com").addConverterFactory(GsonConverterFactory.create()).build();
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        ForgotPwdRequest forgotPwdRequest = new ForgotPwdRequest(recoveryEmail);
+        apiService.sendRecoveryEmail(forgotPwdRequest).enqueue(new Callback<ApiResponse>() {
+            @Override
+            public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+                if (response.isSuccessful()){
+                    ApiResponse apiResponse = response.body();
+                    if (apiResponse != null && apiResponse.success){
+                        Toast.makeText(ForgotPwdActivity.this, "Email sent successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ForgotPwdActivity.this, CheckMailActivity.class));
+                    }
+                }
+                else{
+                    Log.e("Error", "Request failed: " + response.code() + " " + response.message());
+                    try{
+                        String errorBody = response.errorBody().string();
+                        Log.e("Error", "Error body: " + errorBody);
+                    }
+                    catch (Exception e){
+                        Log.e("Error", "Error parsing error body", e);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ApiResponse> call, Throwable t) {
+                Log.e("Error", "Network error", t);
+            }
+        });
+
+    }
+
+    interface ApiService{
+        @POST("api/potholescanner/forgotpassword")
+        Call<ApiResponse> sendRecoveryEmail(@Body ForgotPwdRequest forgotPwdRequest);
+    }
+    class ForgotPwdRequest{
+        String email;
+        public ForgotPwdRequest(String email){
+            this.email = email;
+        }
     }
 }
